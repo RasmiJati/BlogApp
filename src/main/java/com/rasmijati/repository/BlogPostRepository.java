1c64134b6b0876a4b62a1b53426297c0e44af20f
Rasmi Jati <jatirasmi65@gmail.com>
2024-07-06 18:22:55 +0545

    created user, blogpost, comment repository

---
+++ src/main/java/com/rasmijati/repository/BlogPostRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rasmijati.repository;
+
+import com.rasmijati.model.BlogPost;
+import com.rasmijati.model.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author admin
+ */
+public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
+
+    List<BlogPost> findByUser(User user);
+}
---
+++ src/main/java/com/rasmijati/repository/CommentRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rasmijati.repository;
+
+import com.rasmijati.model.BlogPost;
+import com.rasmijati.model.Comment;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author admin
+ */
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByBlogPost(BlogPost blogPost);
+}
